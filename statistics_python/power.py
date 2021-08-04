016bce15ca282c35f215836813cc1ba399060297
Weiwei Ouyang <oyww710@gmail.com>
2021-08-04 15:38:34 -0700

    Update power.py

--- statistics_python/power.py
+++ statistics_python/power.py
@@ -6,3 +6,15 @@ std_effect = proportion_effectsize(.20, .25)
 from statsmodels.stats.power import  zt_ind_solve_power
 sample_size = zt_ind_solve_power(effect_size=std_effect, nobs1=None, alpha=.05, power=.80)
 print(sample_size)
+
+
+sample_sizes = np.array(range(5, 100))
+effect_sizes = np.array([0.2, 0.5, 0.8])
+
+# Create results object for t-test analysis
+from statsmodels.stats.power import TTestIndPower
+results = TTestIndPower()
+
+# Plot the power analysis
+results.plot_power(dep_var='nobs', nobs=sample_sizes, effect_size=effect_sizes)
+plt.show()
