5f1a3513b60bf9d2d58c27369c797f9bb1ae03ba
Weiwei Ouyang <oyww710@gmail.com>
2021-08-04 13:45:28 -0700

    Update sample_number.py

--- statistics_python/sample_number.py
+++ statistics_python/sample_number.py
@@ -13,3 +13,10 @@ large = randint(1, 7, 1000)
 # Calculate and print the mean of the large sample
 large_mean = large.mean()
 print(large_mean)
+
+# Adapt code for 100 samples of size 30
+means = [randint(1, 7, 30).mean() for i in range(100)]
+
+# Create and show a histogram of the means
+plt.hist(means)
+plt.show()
