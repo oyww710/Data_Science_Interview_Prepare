934b8a2a8afc86f9935d690e3d00ee1abe4bbe3a
Weiwei Ouyang <oyww710@gmail.com>
2021-08-04 16:09:00 -0700

    Update ecdf.py

--- statistics_python/ecdf.py
+++ statistics_python/ecdf.py
@@ -16,3 +16,21 @@ an empirical distribution function is the distribution function associated with
 the empirical measure of a sample. This cumulative distribution function is a step 
 function that jumps up by 1/n at each of the n data points
 '''
+
+# Compute ECDFs
+x_set, y_set = ecdf(setosa_petal_length)
+x_vers, y_vers = ecdf(versicolor_petal_length)
+x_virg, y_virg = ecdf(virginica_petal_length)
+
+# Plot all ECDFs on the same plot
+_ = plt.plot(x_set, y_set, marker='.', linestyle='none')
+_ = plt.plot(x_vers, y_vers, marker='.', linestyle='none')
+_ = plt.plot(x_virg, y_virg, marker='.', linestyle='none')
+
+# Annotate the plot
+_ = plt.legend(('setosa', 'versicolor', 'virginica'), loc='lower right')
+_ = plt.xlabel('petal length (cm)')
+_ = plt.ylabel('ECDF')
+
+# Display the plot
+plt.show()
