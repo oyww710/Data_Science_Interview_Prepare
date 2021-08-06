a6ad59735b60f9500f7d6131fec048e5f5c3b4f9
Weiwei Ouyang <oyww710@gmail.com>
2021-08-05 20:21:34 -0700

    Update distribution.py

--- statistics_python/distribution.py
+++ statistics_python/distribution.py
@@ -71,3 +71,16 @@ for i in range(3):
     # Print results
     print('n =', n[i], 'Binom:', np.mean(samples_binomial),
                                  np.std(samples_binomial))
+
+    
+# Draw 10,000 samples out of Poisson distribution: n_nohitters
+n_nohitters = np.random.poisson(251/115, size=10000)
+
+# Compute number of samples that are seven or greater: n_large
+n_large = np.sum(n_nohitters >= 7)
+
+# Compute probability of getting seven or more: p_large
+p_large = n_large / 10000
+
+# Print the result
+print('Probability of seven or more no-hitters:', p_large)    
